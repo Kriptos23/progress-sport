accb52d13168b5bc87e661e2c17c24ac1bdd28b3
Kriptos23 <113576598+Kriptos23@users.noreply.github.com>
2024-01-13 16:23:51 -0800

    FIRST CHECK

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(backgroundColor: Colors.grey.shade700, title:
+          Text('write and train')),
+          backgroundColor: Colors.grey.shade900,
+          body: const SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                children: <Widget>[
+                  Center(
+                    child: Text('Your exercise progress'),
+                  )
+                ]
+              ),
+            ),
+          ),
+        ),
+      )
+  );
+}
